9f3e21e033298414630d7605fd13def287c0bacd
Delaine <delainewtan@gmail.com>
2023-11-02 21:02:51 -0700

    Add player and camera reset button, and collision toggle.

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -9,23 +9,36 @@ public class PlayerCamera : MonoBehaviour
 
     float xRotation;
     float yRotation;
-    // Start is called before the first frame update
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Get mouse input for rotation
+        // Check if the reset key is pressed on the keyboard (Home key) or gamepad (Fire1 button, e.g., A on Xbox controller)
+        if (Input.GetKeyDown(KeyCode.Home) || Input.GetButtonDown("Fire1"))
+        {
+            // Reset camera rotation
+            xRotation = 0f;
+            yRotation = 0f;
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+            // Reset the mouse input axes to zero
+            Input.ResetInputAxes();
+        }
+
+        // Process mouse input for rotation
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
         yRotation += mouseX;
         xRotation -= mouseY;
+
         // Camera rotation and orientation
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-    }   
+    }
 }
